728aef5f26cc81bc49af28e53e2b0ceffb9217ca
Jhonata <gutemberg.jhonata@gmail.com>
2021-06-12 16:56:55 -0300

    Add responsiveness to UserList

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,13 +1,14 @@
-import { HStack, Box } from "@chakra-ui/react";
+import { Stack, HStack, Box } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
   return (
-    <HStack
+    <Stack
+      direction={["column", "row"]}
+      spacing="6"
       mt="8"
       justify="space-between"
       align="center"
-      spacing="6"
     >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
@@ -21,6 +22,6 @@ export function Pagination() {
         <PaginationItem number={5} />
         <PaginationItem number={6} />
       </HStack>
-    </HStack>
+    </Stack>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,8 @@ import {
   Th,
   Checkbox,
   Td,
-  Text
+  Text,
+  useBreakpointValue
 } from '@chakra-ui/react';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
@@ -21,10 +22,12 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({ base: false, lg: true })
+
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px={["4", "6"]}>
         <Sidebar />
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
@@ -45,39 +48,27 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["0", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td>
+                <Td px={["0", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Jhonata Gutemberg</Text>
                     <Text fontSize="sm" color="gray.300">
-                      gutemberg.honata@gmail.com
+                      gutemberg.jhonata@gmail.com
                     </Text>
                   </Box>
                 </Td>
-                <Td>12 de Junho, 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>12 de Junho, 2021</Td>}
               </Tr>
             </Tbody>
           </Table>
